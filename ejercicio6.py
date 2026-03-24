a336e5e095d77e386b65d5c24cc8311a9e1a1659
mateotr240 <mateotr24033@gmail.com>
2026-03-24 10:31:19 -0500

    Delete ejercicio6.py

--- ejercicio6.py
+++
@@ -1,9 +0,0 @@
-def suma_primeros_pares(n):
-    if n < 2:
-        return 0
-    if n % 2 == 0:
-        return n + suma_primeros_pares(n - 2)
-    else:
-        return suma_primeros_pares(n - 1)
-resultado = suma_primeros_pares(5)
-print(resultado)
