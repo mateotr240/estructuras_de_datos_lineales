a42fea705df60fc920ff72b37932725a44a8d703
mateotr240 <mateotr24033@gmail.com>
2026-03-24 10:31:07 -0500

    Delete ejercicio4.py

--- ejercicio4.py
+++
@@ -1,7 +0,0 @@
-def calcular_la_multiplicacion(numero1, numero2):
-    if numero2 == 0:
-        return 0
-    else:
-        return numero1 + calcular_la_multiplicacion(numero1, numero2 - 1)
-resultado = calcular_la_multiplicacion(4, 3)
-print(resultado)
